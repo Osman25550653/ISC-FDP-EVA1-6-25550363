e68c40b8f019a11d3abd393352a11254b990a423
Osman25550653 <osmangael56@gmail.com>
2025-08-31 15:19:10 -0600

    Add files via upload

---
+++ EVA1_6/src/main/java/com/mycompany/eva1_6/EVA1_6.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_6;
+
+/**
+ *
+ * @author osman
+ */
+public class EVA1_6 {
+
+    public static void main(String[] args) {
+        double area, radio, pi;
+        pi = 3.1416;
+        radio = 5;
+        area = pi * radio * radio;
+        System.out.println("El valor de un circulo de radio 5 es:");
+        System.out.println(area);
+        radio = 100;
+        area = pi *radio *radio;
+        System.out.println("El valor de un circulo de radio 100 es:");
+        System.out.println(area);
+        
+    }
+}
